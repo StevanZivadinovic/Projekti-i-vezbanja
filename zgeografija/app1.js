9f2dc31f66d4bb603c0c4aac807692052937e601
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-05-21 19:31:29 +0200

    git commit -m'unos'

--- zgeografija/app1.js
+++ zgeografija/app1.js
@@ -1,9 +1,35 @@
 import {Korisnik} from './korisnik.js';
 
+/*
 
-let linkZaPromenuStranice = document.querySelector('#promenaStranice1');
 
 if(!localStorage.username){
-    linkZaPromenuStranice.style =''
+    linkZaPromenuStranice.style ='href = "https://www.youtube.com/watch?v=rKMlRPa1Oa8"';
 }
+
+if(localStorage.username){
+    linkZaPromenuStranice.style ='href = https://www.youtube.com/watch?v=rKMlRPa1Oa8';
+}
+*/
+let linkZaPromenuStranice = document.querySelector('#promenaStranice1');
+let checkUser = () => {
+    if (!localStorage.username) {
+        linkZaPromenuStranice.classList.add("onemoguci");
+    //lihof.classList.add("disabled");
+    
+    } 
   
+    };
+  
+let checkUser2 = () =>{
+  if (localStorage.username) {
+        linkZaPromenuStranice.classList.remove('onemoguci');
+    }
+    return localStorage.username;
+}
+
+
+    checkUser();
+    checkUser2();
+
+   
