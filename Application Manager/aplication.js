f70350cb1e1032d9ecf54f067b208dda4d27dfb6
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-06 20:14:15 +0200

    git commit -m'ne moze nam niko nista'

--- Application Manager/aplication.js
+++ Application Manager/aplication.js
@@ -40,51 +40,128 @@ apply.addEventListener("click", (a) => {
     });
 });
 
-// Prikaz svih dokumenata iz kolekcije
+//Dodeljivanje ID, prikaz svih dokumenata iz kolekcije i brisanje
 
 let divPri = document.querySelector("#prikazDiv");
+divPri.style.backgroundColor = "lightblue";
+divPri.style.border = "2px solid black";
+divPri.style.borderRadius = "6px";
+divPri.style.margin = "5px 0 0 0";
+divPri.style.listStyleType = "none";
+divPri.style.padding = 0;
+let dodeljivanjeId = (doc) => {
+  let li = document.createElement("li");
 
-prikaz.addEventListener("click", (a) => {
-  let podaci = db.collection("podaci");
-  podaci
-    .get()
-    .then((snapshot) => {
-      snapshot.docs.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-        divPri.setAttribute("data-id", doc.id);
+  li.setAttribute("data-id", doc.id);
+  li.style.hover = "background-color:red";
 
-        divPri.innerHTML += `<div style='cursor:pointer; display:inline'>Name: ${
-          doc.data().name
-        }</div><span class='krstic' style='float:right; cursor:pointer;' >x</span>
-       
-        <br>Email: ${
-            doc.data().email
-          }
-        
-        <hr style="height:1px;border:1px solid black;color:#333;background-color:red;">`;
-      });
-    })
-    .catch((error) => {
-      console.error("Cannot get documents from collection: ", error);
-    });
-});
+  let x = document.createElement("div");
 
-//delete document
-let cross = document.querySelectorAll(".krstic");
+  x.textContent = `x`;
+  x.style.float = "right";
+  x.style.backgroundColor = "white";
+  x.style.padding = "3px 3px";
+  li.appendChild(x);
+  li.innerHTML += `${doc.data().name}<br>`;
 
-cross.forEach((a) => {
+  li.innerHTML += `${doc.data().email}<hr>`;
+
+  let a = li.firstChild; //ovo je u stvari x znak, samo sam ga ovako dogvatio, posto je prvo dete li-a
+  li.style.cursor = "pointer";
+
+  divPri.appendChild(li);
+
   a.addEventListener("click", (e) => {
-    let id = e.target.parentElement.getAttribut("data-id");
-    console.log(id);
-
-    db.collection("podaci")
-      .doc(id)
-      .delete()
-      .then(() => {
-        console.log("Zadatak izbrisan");
-      })
-      .catch((error) => {
-        console.log(`Nemoguce obrisati dokument: ${error}`);
-      });
+    e.stopPropagation();
+    let r = confirm("Da li želite da trajno obrišete poruku?");
+    if (r) {
+      let id = e.target.parentElement.getAttribute("data-id");
+      db.collection("podaci").doc(id).delete();
+    }
   });
+};
+
+//dodeljujes id svakom elementu preko funkcije za dodeljivanje id-a, to je funkcija dodeljivanjeId.
+//ovde prolazis kroz svaki dokument i ubacujes ga u gunkciju za dodeljivanje id koju si iznad definisao
+
+db.collection("podaci")
+  .get()
+  .then((snapshot) => {
+    console.log(snapshot.docs); //dobijes sva dokumenta u nizu, ali ne vidis njihov sadrzaj
+    snapshot.docs.forEach((doc) => {
+      // console.log(doc); //dobijes sve dokumente, svaki pojedinacno, ali opet ne vidis sadrzaj
+      // console.log(doc.data()); //ovde tek vidis sazdraj!!!
+      // console.log(doc.data().name);
+      console.log(doc.id);
+
+      dodeljivanjeId(doc);
+    });
+  });
+
+//update
+
+update.addEventListener('click',e=>{
+  let divPri = document.querySelector("#prikazDiv");
+  divPri.innerHTML=``;
+
+  
+
+divPri.style.backgroundColor = "lightblue";
+divPri.style.border = "2px solid black";
+divPri.style.borderRadius = "6px";
+divPri.style.margin = "5px 0 0 0";
+divPri.style.listStyleType = "none";
+divPri.style.padding = 0;
+let dodeljivanjeId = (doc) => {
+  let li = document.createElement("li");
+
+  li.setAttribute("data-id", doc.id);
+  li.style.hover = "background-color:red";
+
+  let x = document.createElement("div");
+
+  x.textContent = `x`;
+  x.style.float = "right";
+  x.style.backgroundColor = "white";
+  x.style.padding = "3px 3px";
+  li.appendChild(x);
+  li.innerHTML += `${doc.data().name}<br>`;
+
+  li.innerHTML += `${doc.data().email}<hr>`;
+
+  let a = li.firstChild; //ovo je u stvari x znak, samo sam ga ovako dogvatio, posto je prvo dete li-a
+  li.style.cursor = "pointer";
+
+  divPri.appendChild(li);
+
+  a.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let r = confirm("Da li želite da trajno obrišete poruku?");
+    if (r) {
+      let id = e.target.parentElement.getAttribute("data-id");
+      db.collection("podaci").doc(id).delete();
+    }
+  });
+};
+
+//dodeljujes id svakom elementu preko funkcije za dodeljivanje id-a, to je funkcija dodeljivanjeId.
+//ovde prolazis kroz svaki dokument i ubacujes ga u gunkciju za dodeljivanje id koju si iznad definisao
+
+db.collection("podaci")
+  .get()
+  .then((snapshot) => {
+    console.log(snapshot.docs); //dobijes sva dokumenta u nizu, ali ne vidis njihov sadrzaj
+    snapshot.docs.forEach((doc) => {
+      // console.log(doc); //dobijes sve dokumente, svaki pojedinacno, ali opet ne vidis sadrzaj
+      // console.log(doc.data()); //ovde tek vidis sazdraj!!!
+      // console.log(doc.data().name);
+      console.log(doc.id);
+
+      dodeljivanjeId(doc);
+    });
+  });
+
+
+
+
 });
