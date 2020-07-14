ca2bf16faa465cacd984a22b952e380fb7c8e3a4
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-15 00:41:43 +0200

    git commit -m'push'

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -58,6 +58,49 @@ console.log(sugarHigh(a, 20));
 
 
 
+//selectors
+
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+
+//Event listeners
+
+todoButton.addEventListener('click',addTodo){
+
+}
+
+let addTodo=e=>{
+  e.preventDafault()
+
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  const newTodo = document.createElement('li');
+  newTodo.innerText='hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = `<i class='fas fa-check></i>`;
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);  
+
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = `<i class='fas fa-trash></i>`;
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton );  
+
+  
+}
+
+//Functions
+
+
+
+
+
 
 
 
