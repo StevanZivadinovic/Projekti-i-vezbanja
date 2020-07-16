06f36b7d84cecc406eeae66938b46f9088ffc5b0
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-17 01:16:29 +0200

    git commit -m'izmena'

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -4,6 +4,8 @@ const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector('.filter-todo');
 
 //Event listeners
+
+
 todoButton.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("haj");
@@ -17,6 +19,10 @@ todoButton.addEventListener("click", (e) => {
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
+  //add todo to localStorage
+
+  saveLocalTodos(todoInput.value);
+//dovde je dodavanje u localStorage
   let completedButton = document.createElement("button");
   completedButton.innerHTML = `<i class="fa fa-check" aria-hidden="true"></i>`;
   completedButton.classList.add("complete-btn");
@@ -47,6 +53,7 @@ console.log('haj2');
     //animation
 
     todo.classList.add("fall");
+    removeLocalTodos(todo);
     todo.addEventListener("transitionend", e=> {
         todo.remove();
     //todo.remove();
@@ -128,5 +135,82 @@ function filterTodo(e) {
 
 
 
+
+let  saveLocalTodos=(todo)=>{
+let todos;
+if(localStorage.getItem('todos')===null){
+  todos=[]
+}
+else{
+  todos=JSON.parse(localStorage.getItem('todos'));
+}
+todos.push(todo);
+localStorage.setItem('todos', JSON.stringify(todos));
+
+}
+//JSON.parse
+//JSON.stringify
+
+
+//preuzimas nakon refrisiranja stranice podatke prethodno unesene u localStorage
+
+let getTodos=()=>{
+  let todos;
+  if(localStorage.getItem('todos')===null){
+    todos=[]
+  }
+  else{
+    todos=JSON.parse(localStorage.getItem('todos'));
+  }
+
+  todos.forEach(function(todo){
+    let todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+  
+    let newTodo = document.createElement("li");
+  
+    newTodo.innerText = todo;//ovaj red je izmenjen u odnosu na gore
+
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+  
+   
+    let completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fa fa-check" aria-hidden="true"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    console.log(completedButton);
+    let trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fa fa-trash" aria-hidden="true"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded',getTodos);
+
+
+//brisanje
+
+let removeLocalTodos=todo=>{
+
+  let todos;
+  if(localStorage.getItem('todos')===null){
+    todos=[]
+  }
+  else{
+    todos=JSON.parse(localStorage.getItem('todos'));
+  }
+
+let todoIndex = todo.children[0].innerText;
+todos.splice(todos.indexOf(todoIndex),1)//todoIndex u splace metodi
+//u ovom primeru predstavlja poyiciju elementa niza koji hocemo da uklonimo,
+//a 1 predstavlja koliko elemenata hocemo da uklonimo 
+
+localStorage.setItem('todos', JSON.stringify(todos));
+
+};
 
 
