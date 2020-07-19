bcea72acb5ebeba28d6f0bf2a874d84da34185f4
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-20 00:06:39 +0200

    git commit -m'izmena'

--- React/recipe/src/recipe.js
+++ React/recipe/src/recipe.js
@@ -3,8 +3,8 @@ import style from './recipe.module.css';
 
 let Recipe = (props) => {
   return (
-    <div>
-      <h1 className={style.recipe}>{props.title}</h1>
+    <div className={style.recipe}>
+      <h1 >{props.title}</h1>
       <ol>
         {props.ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
