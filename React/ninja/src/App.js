243848640addf3e9ea038dd2cb1d9c694b5133cf
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-23 22:32:03 +0200

    git commit -m'izmena'

---
+++ React/ninja/src/App.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+
+
+class App extends Component {
+
+
+
+
+  render(){
+    return (
+    <div className="App">
+      <h1>MarioPlan</h1>
+    </div>
+  );
+  }
+   
+
+ 
+}
+
+export default App;
