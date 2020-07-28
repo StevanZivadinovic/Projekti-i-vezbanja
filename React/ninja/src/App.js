2812c817dd5917237fe07382949c929a7656f835
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-28 15:31:05 +0200

    git commit -m'izmena'

--- React/ninja/src/App.js
+++ React/ninja/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/leyout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard.js";
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
+
+
 
 class App extends Component {
   render() {
@@ -10,7 +16,16 @@ class App extends Component {
         <div className="App">
           <Navbar></Navbar>
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />{/*exact znaci da url 
+            mora bas da bude ovo sto se unese za path */}
+            <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateProject} />
+
+
+
+
           </Switch>
         </div>
       </BrowserRouter>
