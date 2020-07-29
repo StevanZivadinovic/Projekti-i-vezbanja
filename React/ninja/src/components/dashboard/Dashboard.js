3da14d5989c97f15cbd52027e3a8aa43cf999e3e
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-07-29 23:20:57 +0200

    git commit -m'izmena'

--- React/ninja/src/components/dashboard/Dashboard.js
+++ React/ninja/src/components/dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
+import {connect} from 'react-redux';
 
 class Dashboard extends Component {
   render() {
+  //console.log(this.props);//ovo console.log vraca sve props-ove iz ove komponente kolko kapiram
+    let {projects} = this.props;
+    console.log(this.props)
     return (
       <div className="dashboard container">
         <div className="row ">
@@ -12,7 +16,9 @@ class Dashboard extends Component {
             </div>
           <div className="col s12 m6">
               
-            <ProjectList></ProjectList>
+            <ProjectList projects={projects}></ProjectList>{/*Ovo {projects} je ovo projects dole
+            iz funkcije mapStateToProps, preuzeto iz projectReducer.js, a onda preko connect ugradjene
+            funkcije ubaceno u Dashboard */}
           </div>
           <div className="col s12 m5 offset-m1"></div>
         </div>
@@ -21,4 +27,11 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+let mapStateToProps = (state) =>{
+  return{//ovde idu uglaste zagrade, specifican slucaj, obrati paznju, razradi
+    projects:state.project.projects
+
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
--- React/ninja/src/index.js
+++ React/ninja/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import rootReducer from './components/store/reducers/rootReducer';
+import {Provider} from 'react-redux';//ovo sluzi da spoji react i redux, to je kao neki povezujuci sloj
+
+let store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode><Provider store={store}><App /></Provider>
+ 
   </React.StrictMode>,
   document.getElementById('root')
 );
