f2c6b8de15772a7067637324b01cbc5a71d7937f
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-08-19 22:06:18 +0200

    git commit -m''

--- React/accounts/main.js
+++ React/accounts/main.js
@@ -1,7 +1,40 @@
 
 let accountsTableBody = document.querySelector('#accounts-table-body');
+let allLinks = document.querySelector('.nav-link');
 let accountViewBtn = document.querySelector('[href="accounts-view"]');
-console.log(accountViewBtn);
+let addAccountViewBtn = document.querySelector('[href="add-accounts-view"]');
+let accountsView = document.querySelector('#accounts-view');
+let addAccountView = document.querySelector('#add-account-view');
+let views = document.querySelector('.view');
+
+let showView = (e) =>{
+    e.preventDefault();
+    for(let i = 0; i<views.length; i++){
+        views[i].style.display = 'none';
+    }
+}
+
+    for(let i = 0; i<allLinks.length; i++){
+        allLinks[i].addEventListener('click', showView);
+    }
+
+
+
+addAccountViewBtn.addEventListener('click',e=>{
+    e.preventDefault();
+    addAccountView.style.display = 'block';
+    accountsView.style.display = 'none';
+})
+
+accountViewBtn.addEventListener('click',e=>{
+    e.preventDefault();
+    addAccountView.style.display = 'none';
+    accountsView.style.display = 'block';
+})
+
+
+
+
 
 function createAccountsTable(){
     let htmlAccounts =``;
