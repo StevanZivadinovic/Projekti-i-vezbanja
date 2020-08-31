424ac43837b16ef18bb56fe0393c7b099a517105
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-08-31 21:59:17 +0200

    git commit -m''

---
+++ React/iks-oks/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NameTag from "./components/nameTag";
+
+const initMatrix = [];
+function App() {
+  const [matrix, setMatrix] = useState(initMatrix);
+  const [matrixSize, setMatrixSize] = useState(3);
+  const [currentPlayer, setCurrentPlayer] = useState("o");
+
+  useEffect(() => {
+    const row = new Array(matrixSize).fill(null);
+
+    const tempMatrix = [];
+
+    for (let i = 0; i < matrixSize; i++) {
+      tempMatrix.push([...row]);
+    }
+    setMatrix(tempMatrix);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          {
+          matrix.map((val, c) => (
+            <div className='c'>
+            {val.map(()=>( 
+              <div className='r'>hi</div>
+            ))}
+             
+              
+            
+              
+               
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
