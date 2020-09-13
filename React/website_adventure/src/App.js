71e0fd5cd284cd4edce643127ef4452d10678a63
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-09-13 18:34:27 +0200

    git commit -m""

--- React/website_adventure/src/App.js
+++ React/website_adventure/src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
 import "./App.css";
-import Nav from "./components/nav";
-import About from "./components/about";
-import Home from "./components/home";
-import Shop from './components/shop'
+import Navbar from './components/navbar'
+
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav></Nav>
-       <Route exact path='/' component={Home}/>
-       <Route exact path='/about' component={About}/>
-       <Route exact path='/shop' component={Shop}/>
-
-
-      </div>
-    </Router>
+    <div className='App'>
+      <Router>
+      <Navbar></Navbar>
+      <Route path='/' exact></Route>
+      
+      </Router>
+    </div>
   );
 }
 
---
+++ React/website_adventure/src/components/navbar.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Navbar() {
+
+    const [click, setClick] = useState();
+
+    return (
+        <div>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                   
+                <Link to='/' className='navbar-logo'>
+                    TRVL <i className='fab fa-typo3'></i>
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
