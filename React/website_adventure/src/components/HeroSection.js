39dda148f5f9c4cebcb661a22962caf6743bcffa
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-09-15 12:38:04 +0200

    git commit -m""

--- React/website_adventure/src/components/HeroSection.js
+++ React/website_adventure/src/components/HeroSection.js
@@ -12,7 +12,7 @@ function HeroSection() {
             <h1>ADWENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
-              <p>text1</p>
+              <p>text</p>
                 <Button 
                 className='btns'
                 buttonStyle='btn--outline'
