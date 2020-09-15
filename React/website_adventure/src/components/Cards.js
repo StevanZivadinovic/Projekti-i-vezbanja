b71fab7553c792ad9f743fe8f4f512e26fba5292
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-09-15 14:38:33 +0200

    git commit -m""

--- React/website_adventure/src/components/Cards.js
+++ React/website_adventure/src/components/Cards.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
 import Image9 from './images/img-9.jpg';
+import Image2 from './images/img-2.jpg';
+import Image3 from './images/img-3.jpg';
+import Image4 from './images/img-4.jpg';
+import Image8 from './images/img-8.jpg';
+
 
 function Cards() {
   return (
@@ -17,7 +22,7 @@ function Cards() {
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src={Image2}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -25,19 +30,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src={Image3}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src={Image4}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src={Image8}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
