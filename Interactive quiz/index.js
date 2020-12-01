5603437dd020ba12de1e81184ded34b6336b7bbc
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-12-01 20:14:15 +0100

    git commit -m""

---
+++ Interactive quiz/index.js
@@ -0,0 +1,13 @@
+console.log(Math);
+
+let a = 1.6;
+
+let b = Math.round(a);
+let c = Math.floor(a);
+let v = Math.ceil(a);
+let d = Math.trunc(a);
+
+console.log(b);
+console.log(c);
+console.log(v);
+console.log(d);
