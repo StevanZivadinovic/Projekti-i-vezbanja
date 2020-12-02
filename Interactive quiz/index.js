87f0bbbc7db2afeea287aebd7477eb64c3a72d11
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-12-02 19:08:26 +0100

    git commit -m""

--- Interactive quiz/index.js
+++ Interactive quiz/index.js
@@ -1,13 +1,27 @@
-console.log(Math);
 
-let a = 1.6;
+let sekunda = document.querySelector('.Sec');
+console.log(sekunda);
+let minuta = document.querySelector('.Min');
+let sat = document.querySelector('.Hour');
 
-let b = Math.round(a);
-let c = Math.floor(a);
-let v = Math.ceil(a);
-let d = Math.trunc(a);
 
-console.log(b);
-console.log(c);
-console.log(v);
-console.log(d);
+
+setInterval(()=>{
+    let date= new Date();
+    let hour = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+secRatio = sec/60;
+minRatio = (secRatio + min)/60;
+hourRatio = (minRatio + hour)/12;
+
+sekunda.style.transform = `rotate(${secRatio*360}deg)`;
+minuta.style.transform = `rotate(${minRatio*360}deg)`;
+sat.style.transform = `rotate(${hourRatio*360}deg)`;
+
+
+
+
+console.log(hour,min,sec);
+},1000)
