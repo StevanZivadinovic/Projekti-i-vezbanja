4f3d330d2e74f43f191dd03614829f8e854407f0
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-12-08 23:12:56 +0100

    git commit -m""

--- Little quiz/quiz.js
+++ Little quiz/quiz.js
@@ -2,43 +2,40 @@ let dugme = document.querySelector(".submit");
 console.log(dugme);
 let form = document.querySelector("form");
 
-let pitanje1 = form.q1.value; //ovo probaj da iskoristis kod aplikacije nazvane 'aplication.html'
+let pitanje1 = form.q1.value;
 let pitanje2 = form.q2.value;
 let pitanje3 = form.q3.value;
 let pitanje4 = form.q4.value;
 console.log(pitanje1);
-let ispis = document.createElement("h2");
-let div=document.querySelector('.finish');
-div.appendChild(ispis);
-
+let div = document.querySelector(".writeResult");
+div.style.display = "none";
 dugme.addEventListener("click", (e) => {
   e.preventDefault();
-  let q1 = form.q1.value; //ovo probaj da iskoristis kod aplikacije nazvane 'aplication.html'
+  let q1 = form.q1.value;
   let q2 = form.q2.value;
   let q3 = form.q3.value;
   let q4 = form.q4.value;
   console.log(q1);
   let odgovori = [q1, q2, q3, q4];
   let tacniOdgovori = ["A", "B", "A", "B"];
- 
 
   let rezultat = 0;
-  
+
   odgovori.forEach((a, i) => {
     if (a == tacniOdgovori[i]) {
       rezultat += 25;
     }
   });
-  
-  // scrollTo(0, 0);
+
+  scrollTo(0, 0);
   let broj = 0;
-    let clock = setInterval(() => {
-        ispis.textContent = `Vaš odgovor je: ${broj}%`;
-        if(broj < rezultat) {
-            broj++;
-        }
-        else {
-            clearInterval(clock);
-        }
-    }, 10);
+  let clock = setInterval(() => {
+    div.style.display = "block";
+    div.innerHTML = `<h2>Linux je ${broj}% za tebe</h2>`;
+    if (broj < rezultat) {
+      broj++;
+    } else {
+      clearInterval(clock);
+    }
+  }, 10);
 });
