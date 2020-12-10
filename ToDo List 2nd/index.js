64ce3e875306ed4667a699d681c6a46ada6d0508
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-12-10 23:12:39 +0100

    git commit 'mĆĆ

---
+++ ToDo List 2nd/index.js
@@ -0,0 +1,45 @@
+let form = document.querySelector("form");
+let dugme = document.querySelector(".dugme");
+let ul = document.querySelector(".lista");
+dugme.addEventListener("click", (e) => {
+  e.preventDefault();
+  let input = form.pas.value.trim().toLowerCase();
+  console.log(input.length);
+  if (input.length && input.length < 15) {
+    ul.innerHTML += `<div class='item'><li class='klasaListe'>${input} </li><button class='trash'><i class="fas fa-trash-alt"></i></button></div>`;
+  }
+});
+
+ul.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("trash") ||
+    e.target.classList.contains("fa-trash-alt")
+  ) {
+    let div = document.querySelector(".item");
+    div.remove();
+  }
+});
+
+let pretraga = document.querySelector(".pretraga");
+pretraga.addEventListener("keyup", (e) => {
+  let result = pretraga.value.trim().toLowerCase();
+
+  let todos = document.querySelector(".lista").children;
+  let todos1 = Array.from(todos);
+
+  todos1
+    .filter((a) => {
+      return !a.firstChild.textContent.includes(result);
+    })
+    .forEach((a) => {
+      a.style.display = "none";
+    });
+
+  todos1
+    .filter((a) => {
+      return a.firstChild.textContent.includes(result);
+    })
+    .forEach((a) => {
+      a.style.display = "flex";
+    });
+});
--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,10 +1,9 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector('.filter-todo');
+const filterOption = document.querySelector(".filter-todo");
 
 //Event listeners
-
 
 todoButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -22,43 +21,40 @@ todoButton.addEventListener("click", (e) => {
   //add todo to localStorage
 
   saveLocalTodos(todoInput.value);
-//dovde je dodavanje u localStorage
+  //dovde je dodavanje u localStorage
   let completedButton = document.createElement("button");
   completedButton.innerHTML = `<i class="fa fa-check" aria-hidden="true"></i>`;
   completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
-  console.log(completedButton);
+ 
   let trashButton = document.createElement("button");
   trashButton.innerHTML = `<i class="fa fa-trash" aria-hidden="true"></i>`;
   trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   todoList.appendChild(todoDiv);
 
-  //cllear todoInput value
+  //clear todoInput value
 
   todoInput.value = "";
 });
-
 
 todoList.addEventListener("click", deleteCheck);
 
 function deleteCheck(e) {
   let item = e.target;
-console.log('haj2');
+  
+
   //delete
 
   if (item.classList[0] === "trash-btn") {
-    //item.remove();
     let todo = item.parentElement;
     //animation
 
     todo.classList.add("fall");
     removeLocalTodos(todo);
-    todo.addEventListener("transitionend", e=> {
-        todo.remove();
-    //todo.remove();
+    todo.addEventListener("transitionend", (e) => {
+      todo.remove();
     });
-  
   }
 
   //check mark
@@ -68,113 +64,68 @@ console.log('haj2');
   }
 }
 
-
-filterOption.addEventListener('click',filterTodo);
-
-
-
-
-/*
-function filterTodo(e){
-const todos = todoList.childNodes;
-console.log(todos);
-todos.forEach(function(todo){
-  console.log(e.target.value);
-  switch(e.target.value){
-    case "all":
-      todo.style.display="flex";
-      break;
-    case "completed":
-      if(todo.classList.contains('completed')){
-        todo.style.display='flex';
-
-      }
-      else{
-        todo.style.display="none";
-      }
-      case "uncompleted":
-      if(!todo.classList.contains('completed')){
-        todo.style.display='flex';
-
-      }
-      else{
-        todo.style.display="none";
-      }
-
-  }
-})
-}
-
-*/
+filterOption.addEventListener("click", filterTodo);
 
 function filterTodo(e) {
-  const todos = document.querySelectorAll('.todo');
+  const todos = document.querySelectorAll(".todo");
   // i finr childrens with selector alll
   todos.forEach((todo) => {
-    switch(e.target.value) {
-      case 'all':
-        todo.style.display = 'flex';
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
         break;
-      case 'completed':
-        if(todo.classList.contains('completed')) {
-          todo.style.display = 'flex';
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
         } else {
-          todo.style.display = 'none';
+          todo.style.display = "none";
         }
         break;
-      case 'uncompleted':
-        if(!todo.classList.contains('completed')) {
-          todo.style.display = 'flex';
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
         } else {
-          todo.style.display = 'none';
+          todo.style.display = "none";
         }
         break;
     }
-  })
+  });
 }
 
-
-
-
-let  saveLocalTodos=(todo)=>{
-let todos;
-if(localStorage.getItem('todos')===null){
-  todos=[]
-}
-else{
-  todos=JSON.parse(localStorage.getItem('todos'));
-}
-todos.push(todo);
-localStorage.setItem('todos', JSON.stringify(todos));
-
-}
+let saveLocalTodos = (todo) => {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 //JSON.parse
 //JSON.stringify
 
-
 //preuzimas nakon refrisiranja stranice podatke prethodno unesene u localStorage
 
-let getTodos=()=>{
+let getTodos = () => {
   let todos;
-  if(localStorage.getItem('todos')===null){
-    todos=[]
-  }
-  else{
-    todos=JSON.parse(localStorage.getItem('todos'));
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
 
-  todos.forEach(function(todo){
+  todos.forEach(function (todo) {
     let todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-  
+
     let newTodo = document.createElement("li");
-  
-    newTodo.innerText = todo;//ovaj red je izmenjen u odnosu na gore
+
+    newTodo.innerText = todo; //ovaj red je izmenjen u odnosu na gore
 
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
-  
-   
+
     let completedButton = document.createElement("button");
     completedButton.innerHTML = `<i class="fa fa-check" aria-hidden="true"></i>`;
     completedButton.classList.add("complete-btn");
@@ -186,31 +137,24 @@ let getTodos=()=>{
     todoDiv.appendChild(trashButton);
     todoList.appendChild(todoDiv);
   });
+};
 
-}
-
-document.addEventListener('DOMContentLoaded',getTodos);
-
+document.addEventListener("DOMContentLoaded", getTodos);
 
 //brisanje
 
-let removeLocalTodos=todo=>{
-
+let removeLocalTodos = (todo) => {
   let todos;
-  if(localStorage.getItem('todos')===null){
-    todos=[]
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
-  else{
-    todos=JSON.parse(localStorage.getItem('todos'));
-  }
 
-let todoIndex = todo.children[0].innerText;
-todos.splice(todos.indexOf(todoIndex),1)//todoIndex u splace metodi
-//u ovom primeru predstavlja poyiciju elementa niza koji hocemo da uklonimo,
-//a 1 predstavlja koliko elemenata hocemo da uklonimo 
+  let todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1); //todoIndex u splace metodi
+  //u ovom primeru predstavlja poyiciju elementa niza koji hocemo da uklonimo,
+  //a 1 predstavlja koliko elemenata hocemo da uklonimo
 
-localStorage.setItem('todos', JSON.stringify(todos));
-
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
-
-
