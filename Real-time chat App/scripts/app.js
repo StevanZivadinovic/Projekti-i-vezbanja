814446b6fa875c41c7dcd084dfbd61f0d76e16a9
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-12-20 22:59:41 +0100

    git commit 'm ""

--- Real-time chat App/scripts/app.js
+++ Real-time chat App/scripts/app.js
@@ -1,12 +1,78 @@
 //dom
-let lista = document.querySelector('.chat-list');
+let lista = document.querySelector(".chat-list");
+let newForm = document.querySelector(".new-chat");
+let newName = document.querySelector(".new-name");
+let ispis = document.querySelector(".ispis");
+let dugmici = document.querySelectorAll(".p");
+console.log(dugmici);
 
-
-let prvi = new Chatroom('gaming', 'stevan');
-let prikazBrowseru = new Ui(lista);
-
-prvi.promenaRoom = 'general';
-prvi.ispis((data)=>{
-     console.log(data.username)
-    prikazBrowseru.render(data);
+dugmici.forEach((a) => {
+  a.addEventListener("click", (e) => {
+    console.log(a.id);
+    let id = a.id;
+    prvi.updateRoom(id);
+    prikazBrowseru.clear();
+    prvi.ispis((data) => {
+      prikazBrowseru.render(data);
+    });
+  });
 });
+
+newForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(prvi.preuzimanjeRoom);
+  let message = newForm.message.value;
+  prvi
+    .addChat(message)
+    .then(() => {
+      newForm.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+newName.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let ime = newName.message.value.trim();
+
+  prvi.updateUsername(ime); //moze i ovako umesto ovog reda ispod
+  // prvi.promeniUsername = ime;
+  prvi.preuzimanjeUsername;
+  ispis.innerHTML = `Your name is updated to: ${ime}`;
+  setTimeout(() => {
+    ispis.innerHTML = "";
+  }, 3000);
+  newName.reset();
+});
+
+let checkUsername = () => {
+  if (localStorage.username) {
+    return localStorage.username;
+  } else {
+    return "anonymous";
+  }
+};
+
+let checkRoom = () => {
+  if (localStorage.room) {
+    return localStorage.room;
+  } else {
+    return "gaming";
+  }
+};
+
+let prikazBrowseru = new Ui(lista);
+let prvi = new Chatroom(checkRoom(), checkUsername());
+
+prvi.ispis((data) => {
+  prikazBrowseru.render(data);
+});
+
+// db.collection('chats').get()
+// .then(snapshot=>{
+//     snapshot.docs.forEach(doc => {
+//       // doc.ref.delete();
+
+//     });
+// });
--- Real-time chat App/scripts/ui.js
+++ Real-time chat App/scripts/ui.js
@@ -2,12 +2,15 @@ class Ui{
     constructor(lista){
         this.lista = lista;
     }
-
+    clear(){
+        lista.innerHTML='';
+    }
     render(data){
-
+        let vreme = dateFns.distanceInWordsToNow(data.created_at.toDate())
         let html = `<li>
-            <span>Ime:${data.username}</span>
-            <span>Poruka:${data.message}</span>
+            <span><b>${data.username}</b> </span>
+            <span>${data.message} </span>
+           <div style='color:lightgrey'>${vreme}</div>
         </li>
         `
         this.lista.innerHTML +=html;
