1e9de317c7d7a1bba74c13bba81cc03895553cc8
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-12-27 20:53:04 +0100

    git commit -m ""

--- Game guides/scripts/script.js
+++ Game guides/scripts/script.js
@@ -1,3 +1,35 @@
+//ispis podataka iz baze u browseru
+
+let guidesList = document.querySelector("#guidesLista");
+
+let ispisPodataka = (data) => {
+  console.log(data);
+  if (data) {
+    let html = ``
+  data.forEach(doc=>{
+    let guide = doc.data();
+    let li =` 
+      <li>
+          <button class="collapsible" id="coll">${guide.title}</button>
+          <div class="collapsible" id="ispis">
+            <p>${guide.content}</p>
+          </div>
+        </li>
+      `;
+      html +=li;
+  })
+      guidesList.innerHTML = html;
+
+
+  
+  
+  } else {
+    guidesList.innerHTML = `<li><h5>Login to view guides</h5></li>`;
+  }
+};
+
+
+
 let navigacija = document.querySelectorAll("#signIn");
 let signIn = document.querySelector("#modal-signup");
 
@@ -9,8 +41,6 @@ let createGuide = document.querySelector("#modal-create");
 
 let account = document.querySelectorAll("#account");
 let modalAccount = document.querySelector("#modal-account");
-
-
 
 navigacija.forEach((a) => {
   a.addEventListener("click", (e) => {
@@ -27,18 +57,18 @@ logedIn.forEach((a) => {
 });
 
 guide.forEach((a) => {
-    a.addEventListener("click", (e) => {
-      e.preventDefault();
-      createGuide.style.display = "block";
-    });
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    createGuide.style.display = "block";
   });
-  
-  account.forEach((a) => {
-    a.addEventListener("click", (e) => {
-      e.preventDefault();
-      modalAccount.style.display = "block";
-    });
+});
+
+account.forEach((a) => {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    modalAccount.style.display = "block";
   });
+});
 
 let iks = document.querySelectorAll(".iks");
 iks.forEach((a) => {
@@ -47,7 +77,5 @@ iks.forEach((a) => {
     logedInForm.style.display = "none";
     createGuide.style.display = "none";
     modalAccount.style.display = "none";
-
-
   });
 });
