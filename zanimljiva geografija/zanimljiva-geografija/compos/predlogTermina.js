57e39e6db57f97de65eb31953d087b80f6d3c715
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-01-28 00:33:32 +0100

    commit

--- zanimljiva geografija/zanimljiva-geografija/compos/predlogTermina.js
+++ zanimljiva geografija/zanimljiva-geografija/compos/predlogTermina.js
@@ -1,8 +1,33 @@
 let vrednostIme = localStorage.getItem("korisnickoIme");
 let ispisKorisnika = document.querySelector(".ispisKorisnika");
-console.log(vrednostIme);
+let input = document.querySelector("input");
+let btn1 = document.querySelector(".btn1");
+let form = document.querySelector('#formTermini');
+
 // provera korisnika na pocetku, da li je prijavljen
 if (localStorage.getItem("korisnickoIme")) {
-    ispisKorisnika.innerText = vrednostIme;
-    
-  } 
+  ispisKorisnika.innerText = vrednostIme;
+}
+
+
+  //validacija inputa
+  let patternUsername = /^[a-z\s]+$/;
+  
+  input.addEventListener("keyup", (e) => {
+    let input = document.querySelector("input");
+    let korisnickoIme = input.value;
+
+    let pom = patternUsername.test(korisnickoIme);
+      console.log(pom);
+    if(pom){
+      btn1.addEventListener('click',e=>{
+        let korisnickoIme = input.value;
+          console.log(korisnickoIme);
+      });
+    }else{
+      console.log('neeeeeee');
+      btn1.style.disabled = true;
+    }
+  });
+
+  
