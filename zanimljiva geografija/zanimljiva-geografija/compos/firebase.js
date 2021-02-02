104eb1a8d100338fc9121c2f730c4ffb6d3b2bfe
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-02-02 14:30:02 +0100

    izmena

--- zanimljiva geografija/zanimljiva-geografija/compos/firebase.js
+++ zanimljiva geografija/zanimljiva-geografija/compos/firebase.js
@@ -1,26 +1,58 @@
-const ispisUbazu = (pojam) => {
-  console.log(pojam);
+const ispisUbazu = (ime, grupa, pocetnoSlovo) => {
+  console.log(ime, grupa, pocetnoSlovo);
 
   let provera;
-  db.collection("pojmovi").onSnapshot((snaps) => {
-    snaps.forEach((doc) => {
-      console.log(doc.data());
-        provera = doc.data()
-    });
+  let kategorija;
+  let vrednost;
+//  let a =()=>{
+//   db.collection("pojmovi")
+//           .add({
+//             kategorija: grupa,
+//             pojam:ime
+//           })
+//           .then((data) => {
+//              console.log("podaci su uneseni");
+//           })
+//           .catch(err=>{
+//             console.log('Postoji takav podatak');
+//           })
+//  } 
 
-    
-  });
-  if((pojam!==provera)){
-    db.collection("pojmovi").add({
-        grad: pojam,
-      });
-  }
-  else{
-      alert('Takav pojam vec postoji')
-  }
+
+  db.collection("pojmovi")
+  .where('kategorija','==',`${grupa}`)
+  .where('pojam','==',`${ime}`)
+  .get()
+  .then(data=>{
+
+   
+      if(data.size>0 ){
+        console.log('neispravno');
+        return false;
+        
+      }
+      else{
+        db.collection("pojmovi").doc()
+          .set({
+            kategorija: grupa,
+            pojam:ime,
+            pocetnoSlovo: pocetnoSlovo,
+            korisnik: localStorage.getItem("korisnickoIme"),
+            vremeKreiranja: new Date().toDateString() +' , '+ new Date().toLocaleTimeString()
+          })
+          .then((data) => {
+             console.log("podaci su uneseni");
+          })
+          .catch(err=>{
+            console.log('Postoji takav podatak');
+          })
+      }
+        
+      
+    })
+
+
   
 };
-
-
 
 export default { ispisUbazu };
--- zanimljiva geografija/zanimljiva-geografija/compos/predlogTermina.js
+++ zanimljiva geografija/zanimljiva-geografija/compos/predlogTermina.js
@@ -1,43 +1,36 @@
-import Ispis from './firebase.js'
+import Ispis from "./firebase.js";
 
 let vrednostIme = localStorage.getItem("korisnickoIme");
 let ispisKorisnika = document.querySelector(".ispisKorisnika");
 let btn1 = document.querySelector(".btn1");
-
-
-
 
 // provera korisnika na pocetku, da li je prijavljen
 if (localStorage.getItem("korisnickoIme")) {
   ispisKorisnika.innerText = vrednostIme;
 }
 
+//validacija inputa
+let patternUsername = /^[a-zA-Z\s]+$/;
 
-  //validacija inputa
-  let patternUsername = /^[a-zA-Z\s]+$/;
-  
-  btn1.addEventListener("click", (e) => {
-    let input = document.querySelector("input");
-    let korisnickoIme = input.value;
-   let pom = patternUsername.test(korisnickoIme)
-   if(pom){
+btn1.addEventListener("click", (e) => {
+  let input = document.querySelector("input");
+  let korisnickoIme = input.value;
+
+  let padajuciMeni = document.querySelector(".PadajuciMeni");
+  let grupa = padajuciMeni.value;
+
+  let pom = patternUsername.test(korisnickoIme);
+  if (pom) {
     let b = korisnickoIme.toLowerCase();
-    let a = b.split(' ');
-    let noviString =''
-    a.forEach(b=>{
+    let pocetnoSlovo= b[0].toUpperCase();
+    let a = b.split(" ");
+    let noviString = "";
+   
+    a.forEach((b) => {
       let prepravljeno = b[0].toUpperCase() + b.slice(1);
-      console.log(prepravljeno);
-       noviString += prepravljeno; 
-
-       
-      
-    })
-    Ispis.ispisUbazu(noviString);
     
-    
- 
-   }
-
-  });
-
-  
+      noviString += prepravljeno;
+    });
+    Ispis.ispisUbazu(noviString, grupa, pocetnoSlovo);
+  }
+});
