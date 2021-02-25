44eaaded241b3fa138a12c78a5c9b728e25953a5
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-02-25 22:08:53 +0100

    izmena

--- Rent-a-car/script/script.js
+++ Rent-a-car/script/script.js
@@ -53,13 +53,34 @@ let carType = document.querySelector('#carType').value;
 let ispis = document.querySelector('.ispis');
 let btnShowCar = document.querySelector('#btnShowCar');
 
+ispis.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if (e.target.classList.contains("dugmeDelete")) {
+      console.log("haj");
+  
+      let id = e.target.parentElement.getAttribute("data-id");
+      console.log(id);
+  
+      db.collection("cars")
+        .doc(id)
+        .delete()
+        .then(() => {
+          console.log("Client is deleted");
+        });
+    }
+})
+
 
 let preuzmi = (data, id) => {
+    console.log(id)
     let html = `<li data-id='${id}'>
       
       <div>brand: ${data.brand}</div>
       <div>model: ${data.model}</div>
       <div>constructionYear: ${data.constructionYear}</div>
+      <div>fuelType: ${data.fuelType}</div>
+      <div>numberOfSeats: ${data.numberOfSeats}</div>
+      <img src=${data.pictureLink} style="width:200px">
       <div>fuelType: ${data.fuelType}</div>
       <button class="dugmeDelete">Delete</button>
       <button class="dugmeUpdate">Update</button>
@@ -68,7 +89,19 @@ let preuzmi = (data, id) => {
     ispis.innerHTML += html;
   };
 
+  let obrisati = (id) => {
+    let li = document.querySelectorAll("li");
+    li.forEach((a) => {
+      let dataId = a.getAttribute("data-id");
+      if (dataId === id) {
+        
+          a.remove();
+        
+      }
+    });
+  };
 
+ 
 btnShowCar.addEventListener('click',(e)=>{
     e.preventDefault()
 
@@ -79,8 +112,16 @@ btnShowCar.addEventListener('click',(e)=>{
             console.log(doc.constructionYear)
 
             if(change.type === 'added'){
-                preuzmi(doc, doc.id)
+                preuzmi(doc, change.doc.id)
             }
+            else if (change.type === "removed") {
+
+                if(confirm('Da li zelite da obrisete korisnika?')){
+          
+                  obrisati(doc.id);
+                  
+                }
+              }
         });
       });
 })
