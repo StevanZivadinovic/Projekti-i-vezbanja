bbb87a593e5efbf29e1b318ad05306b797152874
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-02-27 13:05:29 +0100

    izmena

---
+++ Rent-a-car/script/rent.js
@@ -0,0 +1,6 @@
+let addRentalEvent = document.querySelector('#addRentalEvent');
+let addRentalEventForm = document.querySelector('.addRentalEvent');
+addRentalEvent.addEventListener('click',e=>{
+    console.log('haj')
+    addRentalEventForm.style.display = 'flex';
+})
