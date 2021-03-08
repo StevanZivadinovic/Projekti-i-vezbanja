96cd740c0ac6b6c4d225584038316ed27d2f2d93
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-03-08 22:29:58 +0100

    izmena

--- Weather App/public/scripts/forecast.js
+++ Weather App/public/scripts/forecast.js
@@ -1,10 +1,12 @@
 // https://developer.accuweather.com/
 
+
 //  City Search
 
 let getApi = async (city) => {
   let key = "yQVKeePusHNgflojpG5AfKGm7jMebHub";
-  let base = `https://dataservice.accuweather.com/locations/v1/cities/search`;
+ 
+  let base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
   let queres = `?apikey=${key}&q=${city}`;
   let data = await fetch(base + queres);
   let data1 = await data.json();
@@ -16,7 +18,6 @@ let getWeather = async (id) => {
   let key = "yQVKeePusHNgflojpG5AfKGm7jMebHub";
   let base = "http://dataservice.accuweather.com/currentconditions/v1/";
   let queries = `${id}?apikey=${key}`;
-
   let data = await fetch(base + queries);
   let data1 = await data.json();
   return data1[0];
